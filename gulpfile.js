d1447e8a4e2140e03e0481361f93465fadc7d2ab
lacistijo <labasmazule@gmail.com>
2017-06-04 21:30:47 +0200

    Customize
    Initial version with custom files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
+var pug         = require('gulp-pug');
 var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
+
 
 var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
+
 
 /**
  * Build the Jekyll Site
@@ -18,12 +21,14 @@ gulp.task('jekyll-build', function (done) {
         .on('close', done);
 });
 
+
 /**
  * Rebuild Jekyll & do page reload
  */
 gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
     browserSync.reload();
 });
+
 
 /**
  * Wait for jekyll-build, then launch the Server
@@ -32,33 +37,50 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
     browserSync({
         server: {
             baseDir: '_site'
-        }
+        }/*,
+        notify: false*/
     });
 });
+
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-    return gulp.src('_scss/main.scss')
+    return gulp.src('assets/css/main.scss')
         .pipe(sass({
-            includePaths: ['scss'],
+            includePaths: ['css'],
             onError: browserSync.notify
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
+        .pipe(gulp.dest('_site/assets/css'))
         .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('assets/css'));
 });
+
+
+/**
+ * Adding pug stuff
+ * Watch pug files and compile the into HTML
+ */
+
+gulp.task('pug', function(){
+    return gulp.src('_pugfiles/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('_includes'))
+});
+
 
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch('assets/css/**', ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
+    gulp.watch('_pugfiles/*.pug', ['pug']);
 });
+
 
 /**
  * Default task, running just `gulp` will compile the sass,
